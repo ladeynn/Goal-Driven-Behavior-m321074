450d637c5c36d4c7727f366dae5ed522a9f52b23
ladeynn <85115286+ladeynn@users.noreply.github.com>
2024-11-08 15:50:08 +0000

    Matching Agents with Agents

--- Assets/Scripts/GetPatient.cs
+++ Assets/Scripts/GetPatient.cs
@@ -6,6 +6,8 @@ public class GetPatient : GAction
 {
     public override bool PrePerform()
     {
+        target = GWorld.Instance.RemovePatient();
+        if (target == null) { return false; }
         return true;
     }
 
