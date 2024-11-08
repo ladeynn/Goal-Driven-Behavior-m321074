a6d84c6a362702511e7252a783828ee1fd3f6d55
ladeynn <85115286+ladeynn@users.noreply.github.com>
2024-11-08 17:15:38 +0000

    Moving the Nurse

--- Assets/Scripts/GoToCubicle.cs
+++ Assets/Scripts/GoToCubicle.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GetTreated : GAction
+public class GoToCubicle : GAction
 {
     public override bool PrePerform()
     {
@@ -13,9 +13,10 @@ public class GetTreated : GAction
 
     public override bool PostPerform()
     {
-        GWorld.Instance.GetWorld().ModifyState("isTreated", 1);
+        GWorld.Instance.GetWorld().ModifyState("TreatingPatient", 1);
+        GWorld.Instance.AddCubicles(target);
         inventory.RemoveItem(target);
+        GWorld.Instance.GetWorld().ModifyState("FreeCubicle", 1);
         return true;
     }
 }
-
